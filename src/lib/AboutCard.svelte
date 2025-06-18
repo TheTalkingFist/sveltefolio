38ba3f905a37ac59f233163161c8bfdbc00fbf70
TheTalkingFist <mikhail19871983@gmail.com>
2025-06-18 22:47:43 +0800

    m

--- src/lib/AboutCard.svelte
+++ src/lib/AboutCard.svelte
@@ -1,5 +1,5 @@
 <script>
-
+    import { base } from '$app/paths';
 </script>
 
 <style>
@@ -28,7 +28,7 @@
 </style>
 
 <section class="about">
-    <img src="image/site-deco/FistDoesStuff.png" alt="Mikha'il Daanish" class="profile-image">
+    <img src="{base}/image/site-deco/FistDoesStuff.png" alt="Mikha'il Daanish" class="profile-image">
     <p>
         From the Immersive Applications and Game course in ITE CC, I have a strong foundation in design. I'm always looking for ways to better myself as a game artist and designer.
     </p>
