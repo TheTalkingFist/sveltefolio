1078780c30723db663856277447b0fee009bc50d
thetalkingfist <mikhail19871983@gmail.com>
2025-06-21 09:44:04 +0800

    I honestly don't know

---
+++ src/lib/PageTitle.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { base } from '$app/paths';
+
+    let title;
+    let subtitle;
+</script>
+
+<style>
+
+</style>
+
+<section class="header">
+    <h1 class="title" style="font-family: titleFont;">{title}</h1>
+    <h2 class="subtitle" style="font-family: mainFont;">{subtitle}</h2>
+</section>
--- src/routes/my-games/+page.svelte
+++ src/routes/my-games/+page.svelte
@@ -1,6 +1,12 @@
 <script>
     import { base } from '$app/paths';
     import Footer from '$lib/Footer.svelte';
+    import PageTitle from '$lib/PageTitle.svelte';
 </script>
+
+<PageTitle 
+    title="My Games" 
+    subtitle="Games I've created or contributed to" 
+/>
 
 <Footer />
