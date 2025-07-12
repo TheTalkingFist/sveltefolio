b9fc3b4dc9887ab771aafaf40ed34133cbb7147f
TheTalkingFist <mikhail19871983@gmail.com>
2025-07-12 15:23:16 +0800

    im so tired

--- src/routes/my-games/+page.svelte
+++ src/routes/my-games/+page.svelte
@@ -1,15 +1,7 @@
 <script>
     import { base } from '$app/paths';
     import Footer from '$lib/Footer.svelte';
-    import PageTitle from '$lib/PageTitle.svelte';
-    import GenericGallery from '$lib/GenericGallery.svelte';
 </script>
 
-<PageTitle 
-    title="My Games" 
-    subtitle="Games I've created or contributed to" 
-/>
-
-<GenericGallery />
 
 <Footer />
