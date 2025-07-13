3d396151b1ef224cb8019d19174927bd7ed6f97d
TheTalkingFist <mikhail19871983@gmail.com>
2025-07-13 18:46:26 +0800

    Updates

---
+++ _app/immutable/chunks/NbDCyD69.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1y83wif)==null?void 0:s.base)??"/sveltefolio";var e;const a=((e=globalThis.__sveltekit_1y83wif)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.Cu7tFWDe.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/B8HbU8PA.js";export{o as load_css,r as start};
